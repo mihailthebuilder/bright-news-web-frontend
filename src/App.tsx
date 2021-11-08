468fa359ebb784bf24087cd7b9c46ed1c2b0a987
Mihail Marian <mihailmarian12@gmail.com>
2021-11-08 17:14:22 +0000

    added types to api response

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,30 @@ import { requestUrl } from "./resources/resources";
 
 import "./App.scss";
 
+type ApiUrl = { url: string };
+
+type OkResponse = {
+  url_analyzed: string;
+  website_li: Array<{
+    url: string;
+    score: number;
+  }>;
+  website_raw_data: Array<{
+    text: string;
+    combined_score: number;
+    model1_score: number;
+    model2_score: number;
+  }>;
+};
+
+type BadResponse = {
+  message: string;
+};
+
+type ApiResponse = ApiUrl & (OkResponse | BadResponse);
+
 function App() {
-  const [urlResults, setUrlResults] = useState({});
+  const [urlResults, setUrlResults] = useState<ApiResponse | null>(null);
   const [url, setUrl] = useState("");
   const [page, setPage] = useState("landing");
   const [displayError, setDisplayError] = useState(false);
@@ -32,6 +54,7 @@ function App() {
       })
       .then((res) => {
         if (res.status !== 200) throw new Error("Bad request");
+        console.log(res.data);
 
         setUrlResults(res.data);
         setPage("results");

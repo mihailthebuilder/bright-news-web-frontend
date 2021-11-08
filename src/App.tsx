fcb0957ae2897b197b37a8896c0e79c555a51d4c
Mihail Marian <mihailmarian12@gmail.com>
2021-11-08 17:15:23 +0000

    moved api response type to separate url

--- src/App.tsx
+++ src/App.tsx
@@ -13,28 +13,6 @@ import { requestUrl } from "./resources/resources";
 
 import "./App.scss";
 
-type ApiUrl = { url: string };
-
-type OkResponse = {
-  url_analyzed: string;
-  website_li: Array<{
-    url: string;
-    score: number;
-  }>;
-  website_raw_data: Array<{
-    text: string;
-    combined_score: number;
-    model1_score: number;
-    model2_score: number;
-  }>;
-};
-
-type BadResponse = {
-  message: string;
-};
-
-type ApiResponse = ApiUrl & (OkResponse | BadResponse);
-
 function App() {
   const [urlResults, setUrlResults] = useState<ApiResponse | null>(null);
   const [url, setUrl] = useState("");
